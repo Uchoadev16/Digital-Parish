750299ca626fa1b55bd7b040f3d7324935d6d163
uchoadev16@gmail.com <164269740+Uchoadev16@users.noreply.github.com>
2026-02-08 15:42:37 -0300

    Update Admin.php

--- app/subsystems/gerenciador_paroquias/models/Admin.php
+++ app/subsystems/gerenciador_paroquias/models/Admin.php
@@ -20,6 +20,7 @@ class User extends SelectMain
     public function CadastrarParoquia(
         string $nome_paroquia, 
         string $localizacao, 
+        string $cnpj,
         string $telefone, 
         string $logo,
         string $secretario_nome,
@@ -33,20 +34,36 @@ class User extends SelectMain
         string $vigario_cpf
     ): int
     {
-        $sql = "INSERT INTO {$this->tables['pari']} (nome_paroquia, localizacao, telefone, logo) VALUES (:nome_paroquia, :localizacao, :telefone, :logo)";
-        $stmt = $this->connection->prepare($sql);
-        $stmt->bindParam(':nome_paroquia', $nome_paroquia);
-        $stmt->bindParam(':localizacao', $localizacao);
-        $stmt->bindParam(':telefone', $telefone);
-        $stmt->bindParam(':logo', $logo);
-        $stmt->execute();
-        if (!$stmt->execute()) {
+        $stmt_check_pari = $this->connection->prepare("SELECT * FROM {$this->tables['pari']} WHERE cnpj = :cnpj");
+        $stmt_check_pari->bindParam(':cnpj', $cnpj);
+        if (!$stmt_check_pari->execute()) {
             return 2;
         }
-        
+        if($stmt_check_pari->rowCount() > 0){
+            return 3;
+        }
 
-        $stmt = $this->connection->query("INSERT INTO {$this->tables['prof']} VALUES (null, 'Secretário', $id_paroquia)");
-       
+        $sql = "INSERT INTO {$this->tables['pari']} (nome_paroquia, localizacao, telefone, logo) VALUES (:nome_paroquia, :localizacao, :telefone, :logo)";
+        $stmt_insert_pari = $this->connection->prepare($sql);
+        $stmt_insert_pari->bindParam(':nome_paroquia', $nome_paroquia);
+        $stmt_insert_pari->bindParam(':localizacao', $localizacao);
+        $stmt_insert_pari->bindParam(':telefone', $telefone);
+        $stmt_insert_pari->bindParam(':logo', $logo);
+        $stmt_insert_pari->execute();
+        if (!$stmt_insert_pari->execute()) {
+            return 2;
+        }
+ 
+        $stmt_by_id = $this->connection->prepare("SELECT * FROM {$this->tables['pari']} WHERE cnpj = :cnpj");
+        $stmt_by_id->bindParam(':cnpj', $cnpj);
+        if (!$stmt_by_id->execute()) {
+            return 2;
+        }
+        $id_pari = $stmt_by_id->fetch(PDO::FETCH_ASSOC);
+
+        $stmt = $this->connection->query("INSERT INTO {$this->tables['prof']} VALUES (null, 'Secretário', {$id_pari['id']})");
+        $stmt = $this->connection->query("INSERT INTO {$this->tables['prof']} VALUES (null, 'Padre', {$id_pari['id']})");
+
 
         $sql = "INSERT INTO {$this->tables['users']} (nome_usuario, email, cpf, senha, fk_perfis_id, fk_paroquias_id) VALUES (:nome_usuario, :email, :cpf, :senha, :fk_perfis_id, :fk_paroquias_id)";
         $stmt = $this->connection->prepare($sql);
